519203d69de1a7f40639aa15110e2e2bca8d42bc
D-e-D-a <100025592+D-e-D-a@users.noreply.github.com>
2022-12-10 17:30:21 +0100

    Projekti komponenta

---
+++ src/components/ProjectComponent/ProjectCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ProjectStyle from "../../style/Project.module.scss";
+import btn from "../../style/Button.module.scss";
+
+const ProjectCard = (props) => {
+  const style = ProjectStyle;
+  const btnStyle = btn;
+  return (
+    <div className={style.card}>
+      <div className={style.card_imageHolder}>
+        <img src={props.card.img} alt="slika" />
+      </div>
+      <div className={style.card_container}>
+        <div className={style.text}>
+          <h1 className={style.text_header}>{props.card.name}</h1>
+          <p className={style.text_paragraph}>{props.card.text}</p>
+        </div>
+        <div className={style.card_container_button}>
+          <button className={`${btnStyle} ${style.card_container_button_btn}`}>
+            Read More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
