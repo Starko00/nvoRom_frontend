c02c6f4162cbe1d987cec162a393dec7b18cc81f
D-e-D-a <100025592+D-e-D-a@users.noreply.github.com>
2022-12-17 10:47:53 +0100

    some change

--- src/components/PartnersComponent/Partners.js
+++ src/components/PartnersComponent/Partners.js
@@ -27,7 +27,44 @@ const Partners = () => {
           }}
           loopFillGroupWithBlank={true}
           className="mySwiper Swiper2"
-          BreakPoints
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            280: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            440: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            745: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            924: {
+              slidesPerView: 4,
+              spaceBetween: 10,
+            },
+            1100: {
+              slidesPerView: 4,
+              spaceBetween: 10,
+            },
+            1470: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+            1800: {
+              slidesPerView: 5,
+              spaceBetween: 20,
+            },
+            2300: {
+              slidesPerView: 5,
+              spaceBetween: 20,
+            },
+          }}
         >
           {cardData?.map((card) => {
             return (
