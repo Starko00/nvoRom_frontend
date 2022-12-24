e2d02ab89fcf05324bc5de7c9dcc9d4b07a2e4cf
Starko00 <stefanvukmanovic77@gmail.com>
2022-12-24 18:48:24 +0100

    Projects fully implemented

--- src/components/ProjectPComponent/PP.js
+++ src/components/ProjectPComponent/PP.js
@@ -1,25 +1,29 @@
-
 import PPStyle from "./ProjectPage.module.scss";
 import img from "../images/Image_6.png";
 import btnStyle from "../../style/Button.module.scss";
-import Card from "./cardPP";
-import { cardData } from "../../hooks/ProjectPageHook/cardsData";
-import Text from "./PPText";
+import { CardPP } from "./CardPP";
+
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 const ProjectPage = () => {
   const style = PPStyle;
-  const location = useLocation()
-  const [project,setProject] = useState('')
-  useEffect(()=>{
-    axios.post('http://20.229.216.236/phiramenca/api/v1/getproject',{"_id":location.state.projectId}).then(res=>setProject(res.data.project))
+  const location = useLocation();
+  const [project, setProject] = useState("");
+  useEffect(() => {
+    axios
+      .post("http://20.229.216.236/phiramenca/api/v1/getproject", {
+        _id: location.state.projectId,
+      })
+      .then((res) => {
+        setProject(res.data.project);
+        console.log(res.data.project.documentation.length);
+        
+      });
+  }, []);
 
-  },[])
-  
-  console.log(location)
-  const cardElements = cardData.map((card) => {
-    return <Card card={card} key={card.id} />;
+  const cardElements = project?.documentation?.map((documentation) => {
+    return <CardPP card={documentation} key={documentation} />;
   });
   return (
     <div className={style.container}>
@@ -30,14 +34,16 @@ const ProjectPage = () => {
           alt="About_img"
         />
         <h1 className={style.container_leftSide_header}>Dokumentacija:</h1>
-        <div className={style.container_leftSide_card}>{cardElements}</div>
+        <div className={style.container_leftSide_card}> { project?.documentation && project.documentation?.length != 0 ? cardElements : "No Documentation aveable"  }</div>
       </div>
 
       <div className={style.container_rightSide}>
         <h1 className={style.container_rightSide_header}>
-         {project.projectName}
+          {project.projectName}
         </h1>
-        <p className={style.container_rightSide_paragraph}>{project.primaryText}</p>
+        <p className={style.container_rightSide_paragraph}>
+          {project.primaryText}
+        </p>
         <div className={style.container_rightSide_btncontainer}>
           <button
             className={`${btnStyle} ${style.container_rightSide_btncontainer_button}`}
--- src/components/ProjectPComponent/cardPP.js
+++ src/components/ProjectPComponent/cardPP.js
@@ -1,18 +1,25 @@
 import React from "react";
 import btnStyle from "../../style/Button.module.scss";
 import cardStyle from "../../style/projectsCardSmall.module.scss";
+import { useNavigate } from "react-router-dom";
+import icon from "../../components/images/ic.svg";
 
-const cardAUP = (props) => {
+export const CardPP = (props) => {
+  const navigate = useNavigate()
+  console.log(props)
   const style = cardStyle;
   return (
     <div className={style.card}>
       <div className={style.card_left}>
-        <img className={style.card_left_img} src={props.card.img} alt="Logo" />
-        <h1 className={style.card_left_header}>{props.card.name}</h1>
+        <img className={style.card_left_img} src={icon} alt="Logo" />
+        <h1 className={style.card_left_header}>{props.card.split('----')[1]}</h1>
         <p className={style.card_left_paragraph}>{props.card.date}</p>
       </div>
       <div className={style.card_right}>
-        <button className={`${btnStyle} ${style.card_right_button}`}>
+        
+        <button className={`${btnStyle} ${style.card_right_button}`} onClick ={()=>{
+         window.location.href=`http://20.229.216.236/uploads/projects/${props.card}`
+        }}>
           Read More
         </button>
       </div>
@@ -20,4 +27,4 @@ const cardAUP = (props) => {
   );
 };
 
-export default cardAUP;
+
