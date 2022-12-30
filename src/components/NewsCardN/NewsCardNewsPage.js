1f9ecd70ebf33d97d6f99163a6621882e8e5c9b6
D-e-D-a <100025592+D-e-D-a@users.noreply.github.com>
2022-12-30 13:25:33 +0100

    evis is everywhere

--- src/components/NewsCardN/NewsCardNewsPage.js
+++ src/components/NewsCardN/NewsCardNewsPage.js
@@ -1,29 +1,38 @@
 import React from "react";
 import NewsPStyle from "./NCNP.module.scss";
 import btn from "../../style/Button.module.scss";
+import img from "../images/elvis.jpg";
 
 const CardP = (props) => {
-  
   const style = NewsPStyle;
   const btnstyle = btn;
-  const date =  props.card.date
-
+  const date = props.card.date;
 
   return (
     <div className={style.newscard_container}>
       <div className={style.newscard_container_left}>
         <div className={style.newscard_container_left_imageHolder}>
-          <img src={`http://20.229.216.236/uploads/news/${props.card.photo}`} alt="slika" />
+          <img
+            src={`http://20.229.216.236/uploads/news/${props.card.photo}`}
+            alt="slika"
+          />
         </div>
         <div className={style.newscard_container_left_author}>
           <div className={style.newscard_container_left_author}>
             <img
               className={style.newscard_container_left_author_img}
-              src="https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              src={img}
               alt="slika"
             />
             <p>{props.card.author}</p>
-            <p>{date.split("T")[0].replaceAll("-",".").split(".").reverse().join(".")}</p>
+            <p>
+              {date
+                .split("T")[0]
+                .replaceAll("-", ".")
+                .split(".")
+                .reverse()
+                .join(".")}
+            </p>
           </div>
         </div>
       </div>
