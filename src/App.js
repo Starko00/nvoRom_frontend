862a0af740f325bce1b0884de8d99703bdd7bf7d
Starko00 <stefanvukmanovic77@gmail.com>
2023-02-09 00:41:06 +0100

    1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,30 @@ import NewsPage from "./pages/NewsPageComponent/NewsPage";
 import { ContactPage } from "./pages/ContactPage/ContactPage";
 import { Article } from "./components/NewsPComponent/ArticlePage";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+
 function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCbvCpah1DUZ4L4waf4EG1wCaFRKRI8nLE",
+    authDomain: "firamenca-final.firebaseapp.com",
+    projectId: "firamenca-final",
+    storageBucket: "firamenca-final.appspot.com",
+    messagingSenderId: "243419115084",
+    appId: "1:243419115084:web:46a83b91ad3e2ae2780ee9",
+    measurementId: "G-9WJWCT4H81"
+  };
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
   return (
     <div className="App">
       <div className="app_container">
